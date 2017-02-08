c1e364b10788965ff4381c952d22f33568a95812
Josh Curl <josh@curl.me>
2017-02-07 22:31:27 -0800

    Only update out of date catalogs

---
+++ git/git.go
@@ -0,0 +1,34 @@
+package git
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func Clone(path, url, branch string) error {
+	cmd := exec.Command("git", "clone", "-b", branch, "--single-branch", url, path)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func Update(path, branch string) error {
+	cmd := exec.Command("git", "-C", path, "fetch")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+
+	cmd = exec.Command("git", "-C", path, "checkout", fmt.Sprintf("origin/%s", branch))
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
+}
+
+func HeadCommit(path string) (string, error) {
+	cmd := exec.Command("git", "-C", path, "rev-parse", "HEAD")
+	output, err := cmd.Output()
+	return string(output), err
+}
