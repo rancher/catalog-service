c44f51e33b385d5a847e57b94cdfbb0024d1bfac
Josh Curl <josh@curl.me>
2017-02-09 15:09:42 -0800

    Handle PDEATHSIG

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"syscall"
+
 	log "github.com/Sirupsen/logrus"
 	"github.com/rancher/catalog-service/cmd"
 )
 
 func main() {
+	if _, _, err := syscall.RawSyscall(syscall.SYS_PRCTL, syscall.PR_SET_PDEATHSIG, uintptr(syscall.SIGTERM), 0); err != 0 {
+		log.Fatalf("Failed to set parent death signal: %d", err)
+	}
+
 	if err := cmd.RootCmd.Execute(); err != nil {
 		log.Fatal(err)
 	}
