d97ba770370e6d85690d06c4de80910d30632830
Amy Chen <amy@rancher.com>
2017-06-05 10:33:39 -0700

    changes for project_url and default_version

--- parse/compose.go
+++ parse/compose.go
@@ -7,6 +7,32 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+func TemplateInfo(contents []byte) (model.Template, error) {
+
+	var data map[string]interface{}
+	if err := yaml.Unmarshal([]byte(contents), &data); err != nil {
+		return model.Template{}, err
+	}
+
+	if _, exists := data["projectURL"]; exists {
+		data["project_url"] = data["projectURL"]
+	}
+
+	if _, exists := data["version"]; exists {
+		data["default_version"] = data["version"]
+	} else if _, exists := data["defaultVersion"]; exists {
+		data["default_version"] = data["defaultVersion"]
+	}
+
+	var template model.Template
+	if err := utils.Convert(data, &template); err != nil {
+		return model.Template{}, err
+	}
+
+	return template, nil
+
+}
+
 func CatalogInfoFromTemplateVersion(contents []byte) (model.Version, error) {
 
 	var template model.Version
